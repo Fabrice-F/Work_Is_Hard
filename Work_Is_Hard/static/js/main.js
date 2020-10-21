a4e1321d128f21e17aacfc579de0da8d4ba8da7a
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2020-10-21 16:40:45 +0200

    Ajout de la page creation de poste: Ajout de l'onglet poster quand connect Ajout de la route creationPoste Ajout de la page cereation poste Ajout méthode .js de vérification des champs du poste

--- Work_Is_Hard/static/js/main.js
+++ Work_Is_Hard/static/js/main.js
@@ -1,2 +1,41 @@
-//alert("test");
+function EnvoyerOuPas()
+{
+    TitrePoste = document.getElementById("TitrePoste").value;
+    LienImg = document.getElementById("LienImg").value;
 
+    if(isEmptyOrSpaces(TitrePoste) || isEmptyOrSpaces(LienImg)){
+        alert("Un champs n'est pas renseigné")
+        return false;      
+    }
+    else{
+        
+        alert(TitrePoste);
+        alert(LienImg)
+        return true;
+    }
+}
+
+
+
+function isEmptyOrSpaces(str){
+    return str === null || str.match(/^ *$/) !== null;
+}
+
+function changeApercu(e,baliseChanger)
+{
+    parent = document.getElementById(baliseChanger).parentElement;
+    balise = document.getElementById(baliseChanger);
+    if(baliseChanger =="apercuLegend"){
+        balise.innerHTML=e.value;
+    }
+    else{
+        if((/\.(gif|jpg|jpeg|tiff|png)$/i).test(e.value))
+        {
+            balise.remove();
+            parent.innerHTML +=`<img src="${e.value}" class="gif" alt="test" id="apercuImg" onerror="this.style.display='none'">`;
+    
+        }
+    }
+
+
+}
