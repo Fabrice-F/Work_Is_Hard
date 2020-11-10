0f6c6f85e652afdbd2a92423ac6f182858ee7070
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2020-11-10 22:24:25 +0100

    Suppression méthode champsVide

--- Work_Is_Hard/static/js/inscription.js
+++ Work_Is_Hard/static/js/inscription.js
@@ -100,30 +100,6 @@ function EnvoiFormulaireInscription(){
     }
 }
 
-function ChampsVide(){
-    var nom = document.getElementById("NomFormulaireInscription").value;
-    var prenom = document.getElementById("PrenomFormulaireInscription").value;
-    var pseudo = document.getElementById("PseudoFormulaireInscription").value;
-    const regex = /[A-Za-z0-9]/g;
-
-    if(nom || prenom || pseudo % regex){
-        PrintMessage("msgErrorConfirmationInscription","Les caractères entré dans les champs ne sont pas valide.", true);
-    }else{
-        return false 
-    }
-    
-    if(isEmptyOrSpaces(nom)){
-            PrintMessage("msgErrorConfirmationInscription","Votre champ nom est vide",true);
-            return false;
-    }else if(isEmptyOrSpaces(prenom)){
-        PrintMessage("msgErrorConfirmationInscription","Votre champ prénom est vide",true);
-        return false;
-    }else if(isEmptyOrSpaces(pseudo)){
-        PrintMessage("msgErrorConfirmationInscription","Votre champ pseudo est vide",true);
-        return false;
-    }
-    return false;
-}
 
 function isPasswordFormatCorrect(str)
 {                                      
